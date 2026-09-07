c7543696d49db90be8567f60486f388231579e45
kodyjsch <dakota.schweitzer04@gmail.com>
2026-09-06 22:17:36 -0500

    bobber

--- Assets/Scripts/PlayerFishing.cs
+++ Assets/Scripts/PlayerFishing.cs
@@ -7,12 +7,19 @@ public class PlayerFishing : MonoBehaviour
     public Rigidbody bobber;
     public float speed = 4;
 
+    private bool casted = false;
+
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        if (Input.GetMouseButtonDown(0)) 
         {
-            Rigidbody cast = Instantiate(bobber, transform.position, transform.rotation);
-            cast.velocity = transform.forward * speed;
+            if(casted == false)
+            {
+                casted = true;
+                Rigidbody cast = Instantiate(bobber, transform.position, transform.rotation);
+                cast.velocity = transform.forward * speed;
+                
+            }      
         }
 
     }
