a10824d0fb923c191a79fdcc66c38ce0e91f3eb5
kodyjsch <dakota.schweitzer04@gmail.com>
2026-09-10 16:29:01 -0500

    basically nothing (skull)

--- Assets/Scripts/FishList.cs
+++ Assets/Scripts/FishList.cs
@@ -6,7 +6,9 @@ public class FishList : MonoBehaviour
 {
     public List<GameObject> fishPrefabs;
     public List<Sprite> splashSprites;
+    public List<Sprite> counterSprites;
 
     public List<bool> firstCatch;
+    public List<string> Names;
     public List<string> OneLiners;
 }
