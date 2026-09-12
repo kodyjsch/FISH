bba02e0f58366f61935b037448c8e73fda3971f0
kodyjsch <dakota.schweitzer04@gmail.com>
2026-09-11 21:05:55 -0500

    splash screen summon

--- Assets/Scripts/SplashTrigger.cs
+++ Assets/Scripts/SplashTrigger.cs
@@ -1,13 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class SplashTrigger : MonoBehaviour
 {
+    private FishReturn fRet;
+    private FishList fList;
 
+    public Image SplashSprite;
+    public TextMeshProUGUI Name;
+    public TextMeshProUGUI Zinger;
+
+    public GameObject screen;
+
+    private void Start()
+    {
+        fList = GameObject.Find("FISH LIST (FLIST)").GetComponent<FishList>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("intersected!");
+        if (other.CompareTag("bobber"))
+        {
+            fRet = other.GetComponent<FishReturn>();
+
+            if (fList.firstCatch[fRet.chosenFish] == false)
+            {
+                fList.firstCatch[fRet.chosenFish] = true;
+                SplashSprite.sprite = fList.splashSprites[fRet.chosenFish];
+                Name.text = fList.Names[fRet.chosenFish];
+                Zinger.text = fList.OneLiners[fRet.chosenFish];
+                screen.SetActive(true);
+            }            
+        }
     }
 }
